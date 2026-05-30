fa83e5234117affce3d09230523f6b8b7541b549
kenzoGit02 <kenzorivas11@gmail.com>
2026-05-30 10:44:42 +0800

    test POST route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::post('/test', function(Request $request){
+    Log::info('Received POST request with'. print_r($request->input(), true));
+    return response()->json(['status' => 'ok']);
 });
